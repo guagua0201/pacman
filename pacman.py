a702430ce94a2d2de0f3815e2716b5a0f398da88
guagua0201 <tobyupu@gmail.com>
2019-12-24 08:27:04 +0800

    auto restart after 4 seconds if end

--- pacman.py
+++ pacman.py
@@ -15,44 +15,44 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (10,20)
 
 def main():
 
-	g = game()
-	print(g)
+	
+	while(True): 
+		g = game()
 
-	ms = moveSelector()
+		ms = moveSelector()
 
-	p = pac( g.startX, g.startY )
-
-	while(True):
-		nowMove = ms.getMove(g)
-		if(nowMove == -1):
-			nowMove = g.pac.d
-		if(nowMove == 0):
-			g.pac.move(1,0,g.map,0)
-		elif(nowMove == 1):
-			g.pac.move(0,-1,g.map,1)
-		elif(nowMove == 2):
-			g.pac.move(-1,0,g.map,2)
-		elif(nowMove == 3):
-			g.pac.move(0,1,g.map,3)
-
-		for ghost in g.ghosts:
-			nowMove = ghost.getMove(g)
+		p = pac( g.startX, g.startY )
+		while(True):
+			nowMove = ms.getMove(g)
+			if(nowMove == -1):
+				nowMove = g.pac.d
 			if(nowMove == 0):
-				ghost.move(1,0,g.map,0)
+				g.pac.move(1,0,g.map,0)
 			elif(nowMove == 1):
-				ghost.move(0,-1,g.map,1)
+				g.pac.move(0,-1,g.map,1)
 			elif(nowMove == 2):
-				ghost.move(-1,0,g.map,2)
+				g.pac.move(-1,0,g.map,2)
 			elif(nowMove == 3):
-				ghost.move(0,1,g.map,3)			
+				g.pac.move(0,1,g.map,3)
 
-		state = g.drawAll()
-		if( state == 0 ):
-			print("End! Scores = " + str(g.points) + ", Time = " + str(g.frameCnt//10) + "." + str(g.frameCnt%10))
-			break
-		elif(state == 2):
-			print("Win! Scores = " + str(g.points) + ", Time = " + str(g.frameCnt//10) + "." + str(g.frameCnt%10))
-			break
+			for ghost in g.ghosts:
+				nowMove = ghost.getMove(g)
+				if(nowMove == 0):
+					ghost.move(1,0,g.map,0)
+				elif(nowMove == 1):
+					ghost.move(0,-1,g.map,1)
+				elif(nowMove == 2):
+					ghost.move(-1,0,g.map,2)
+				elif(nowMove == 3):
+					ghost.move(0,1,g.map,3)			
 
+			state = g.drawAll()
+			if( state == 0 ):
+				print("End! Scores = " + str(g.points) + ", Time = " + str(g.frameCnt//10) + "." + str(g.frameCnt%10))
+				break
+			elif(state == 2):
+				print("Win! Scores = " + str(g.points) + ", Time = " + str(g.frameCnt//10) + "." + str(g.frameCnt%10))
+				break
 
+		time.sleep(4)
 main()
